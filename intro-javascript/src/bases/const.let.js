00d2d05fadb25b00993613238e1d6cfacf34581a
Jerson Huayta <jerson.huayta@tismart.com>
2021-09-14 19:53:24 -0500

    clase 1409

---
+++ intro-javascript/src/bases/const.let.js
@@ -0,0 +1,15 @@
+
+
+//Variables y constantes
+
+const nombre = "Jerson";
+const apellido = "Huayta";
+
+var edad = 0;
+const anioNacimiento= 1998;
+const anioActual= 2021;
+
+if(anioActual == 2021){
+    edad =anioActual - anioNacimiento
+}
+console.log('Edad actual : '+edad)
---
+++ intro-javascript/src/index.js
@@ -0,0 +1,20 @@
+
+
+//Variables y constantes
+
+const nombre = "Jerson";
+const apellido = "Huayta";
+
+var edad = 0;
+const anioNacimiento= 1998;
+const anioActual= 2021;
+
+if(anioActual == 2021){
+    let edad = 0;
+    edad =anioActual - anioNacimiento;
+    console.log('Edad actual entorno if : '+edad);
+}
+else{
+
+}
+console.log('Edad actual  entorno global: '+edad);
